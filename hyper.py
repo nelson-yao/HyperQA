ee322540dd8c20b712a8f108ec58dac96315b18c
Nelson Yao <nelson.yao@nuance.com>
2019-04-01 17:06:23 -0400

    forward pass implementaiton

--- hyper.py
+++ hyper.py
@@ -1,21 +1,25 @@
 import tensorflow as tf
-import tensorflow.keras.backend as K
 
 
 class Hyper_Model(tf.keras.Model):
-    def __init__(self, embedding_matrix=None, hinge_margin=0.0):
-        super(HYPER, self).__init__()
-        self.hinge_margin = margin
+    def __init__(self, vocab_size, embedding_matrix=None, hinge_margin=0.0, ):
+        super(Hyper_Model, self).__init__()
+        self.hinge_margin = hinge_margin
         if embedding_matrix:
-            self.embedding = tf.keras.Embedding(300, embeddings_initializer=tf.keras.initializers.Constant(values=embedding_matrix))
+
+            self.embedding = tf.keras.layers.Embedding(vocab_size, output_dim=300, embeddings_initializer=tf.keras.initializers.Constant(value=embedding_matrix))
         else:
-            self.embedding = tf.keras.Embedding(300, embeddings_initializer="uniform")
+            self.embedding = tf.keras.layers.Embedding(vocab_size, output_dim=300, embeddings_initializer="uniform")
         self.projection = tf.keras.layers.Dense(300, activation="relu")
         self.representation = tf.keras.layers.Lambda(self.bow_representation)
         self.distance = tf.keras.layers.Lambda(self.distance)
-        self.wf = tf.Variable(tf.keras.initializer(sh))
+        self.wf = tf.Variable(initial_value=tf.initializers.random_normal()((1,)), dtype=tf.float32)
+        self.bf = tf.Variable(initial_value=tf.initializers.random_normal()((1,)), dtype=tf.float32)
 
-    
+    def call(self, inputs, training=None, mask=None):
+        reference, good, bad = inputs
+        self.
+
     def bow_representation(self, embedding_sequence):
         embedding_sum = tf.squeeze(tf.math.reduce_sum(embedding_sequence, axis=2))
         normalized_sum = embedding_sum / tf.norm(embedding_sum, axis=-1, keep_dims=True)
@@ -27,7 +31,5 @@ class Hyper_Model(tf.keras.Model):
         distance = tf.math.acosh(1 + 2 * num / den)
         return distance
     
-    
-
     
         
