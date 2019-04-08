4071b99c0b07eb20a0c963bbd0640bb8c3c2b654
nelsonyao-mindset <nelson@thinkmindset.com>
2019-04-08 19:49:54 -0400

    debugging forward pass

--- hyper_qa/model.py
+++ hyper_qa/model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 class HyperQA(tf.keras.Model):
     def __init__(self, vocab_size, max_length, embedding_matrix=None):
-        super(HyperQA, self).__init__()
+        super().__init__()
         self.embedding_size = 300
         if embedding_matrix is not None:
             self.embedding = tf.keras.layers.Embedding(vocab_size, output_dim=self.embedding_size, embeddings_initializer=tf.keras.initializers.Constant(value=embedding_matrix))
@@ -21,18 +21,22 @@ class HyperQA(tf.keras.Model):
             reference, good, bad = inputs # each is batch * sequence length * embedding_size
         else:
             reference, good = inputs
-        projection_reference = self.sequence_projection(reference)
-        projection_good = self.sequence_projection(good)
+
+        embedding_ref = self.embedding(reference)
+        embedding_good = self.embedding(good)
+        projection_reference = self.sequence_projection(embedding_ref)
+        projection_good = self.sequence_projection(embedding_good)
 
         sum_reference = tf.reduce_sum(projection_reference,axis=2)
         sum_good = self.bow_representation(projection_good)
-        distance_positive = self.distance(sum_reference, sum_good)
+        distance_positive = self.distance((sum_reference, sum_good))
         similarity_positive = self.wf * distance_positive + self.bf
 
         if training:
-            projection_bad = self.sequence_projection(bad)
+            embedding_bad = self.embedding(bad)
+            projection_bad = self.sequence_projection(embedding_bad)
             sum_bad = self.bow_representation(projection_bad)
-            distance_negative = self.distance(sum_reference, sum_bad)
+            distance_negative = self.distance((sum_reference, sum_bad))
             similarity_negative = self.wf * distance_negative + self.bf
             return similarity_positive, similarity_negative
         else:
@@ -43,7 +47,8 @@ class HyperQA(tf.keras.Model):
         normalized_sum = embedding_sum / tf.norm(embedding_sum, axis=-1, keep_dims=True)
         return normalized_sum
                 
-    def distance(self, q, a):
+    def distance(self, inputs):
+        q, a = inputs
         num = tf.square(tf.norm(q-a, axis=1))
         den = (1 - tf.square(tf.norm(q, axis=1))) * (1 - tf.square(tf.norm(a, axis=1)))
         distance = tf.math.acosh(1 + 2 * num / den)
